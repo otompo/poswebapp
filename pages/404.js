aea9e6f81db18603e352d6109697eab06f2e3140
Abagna <abagnasalifusasco@gmail.com>
2022-09-08 17:44:25 +0000

    latest commit

--- pages/404.js
+++ pages/404.js
@@ -8,10 +8,6 @@ const NotFoundPage = () => {
       <Fragment>
         <div className="page-not-found-wrapper mt-5">
           <h1 id="title_404">404!</h1>
-          <h3 id="description_404">
-            Page Not Found, Have a look at the following post <br />
-            or you can go back to <Link href="/">Homepage</Link>{' '}
-          </h3>
         </div>
       </Fragment>
     </Layout>
